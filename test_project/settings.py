362bff88ee1ae2df1d78e52bd94e8400daccb3f5
MilenkoMarkovic <milenko.markovic@gmail.com>
2024-01-02 06:27:24 +0000

    added db.sqlite

--- test_project/settings.py
+++ test_project/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", default="secret") 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
